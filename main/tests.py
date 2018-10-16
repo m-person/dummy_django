88312eb7cd8159c226de31e31c8eb05d8e1334bd
m-person <andrey@micro-man.com>
2018-10-16 15:34:34 +0300

    buldspec testing

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,10 @@
+from unittest import skip
+
 from django.test import TestCase
 
 
 class FailTestCase(TestCase):
 
+    @skip
     def test_always_fail(self):
         self.fail('This test always fails. For CI/CD testing.')
